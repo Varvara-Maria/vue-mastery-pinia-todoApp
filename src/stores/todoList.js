608b0549ca5c935c57b1865a884ccdd12c98b041
maho1 <mariia.honchariuk@sobaco-betax.com>
2023-09-14 15:57:55 +0200

    mutating-state-with-action

--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -3,5 +3,15 @@ import  { defineStore } from "pinia";
 export const  useTodoListStore = defineStore('todoList', {
     state: () => ({
         todoList: []
-    })
+    }),
+    actions: {
+        addTodo(item) {
+            this.todoList.push({ item, id: this.id++, completed: false })
+        },
+        deleteTodo(itemId) {
+            this.todoList = this.todoList.filter((object) => {
+                return object.id !== itemId
+            })
+        }
+    }
 })
